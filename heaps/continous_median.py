20dcd111c26c27acb547640d6e1e2c9c1e9370bc
Joshua Bizinyika <joshuabiyinzika22@gmail.com>
2023-07-15 16:22:21 +0300

    finished continous median

--- heaps/continous_median.py
+++ heaps/continous_median.py
@@ -1,10 +1,43 @@
+"""
+repeat it
 
-class MedianFinder:
+"""
+
+import heapq
+class MedianFinder(object):
     def __init__(self):
-        pass
+        self.maxHeap = []
+        self.minHeap = []
+        self.median = None
+
 
     def addNum(self, num):
-        pass
+        if len(self.maxHeap) == 0 or num < -self.maxHeap[0]:
+            heapq.heappush(self.maxHeap, -num)
+        else:
+            heapq.heappush(self.minHeap, num)
+        # check if the heaps need to be rebalanced
+        self.rebalanceHeaps()
+        # update median
+        self.updateMedian()
+
+    def rebalanceHeaps(self):
+        if len(self.maxHeap) - len(self.minHeap) == 2:
+            heapq.heappush(self.minHeap, heapq.heappop(self.maxHeap))
+        elif len(self.minHeap) - len(self.maxHeap) == 2:
+            heapq.heappush(self.maxHeap, heapq.heappop(self.minHeap))
+        
+
+    def updateMedian(self):
+        if len(self.minHeap) == len(self.maxHeap):
+            self.median = (self.minHeap[0] + (-self.maxHeap[0]))/2
+        elif len(self.minHeap) > len(self.maxHeap):
+            self.median = self.minHeap[0]
+        else:
+            self.median = -self.maxHeap[0]
+
+       
 
     def findMedian(self):
-        pass
+        return self.median
+    
