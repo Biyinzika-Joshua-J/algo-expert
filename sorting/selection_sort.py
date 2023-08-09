2767e1617d3b327286b0f88a1a30d8cd6a767c14
Joshua Bizinyika <joshuabiyinzika22@gmail.com>
2023-08-09 11:32:16 +0300

    finished selection sort

--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -1,3 +1,18 @@
-def selectionSort(list):
-    pass
+import math
 
+def selectionSort(list):
+    currentIdx = 0
+    while currentIdx < len(list)-1:
+        smallestIdx = currentIdx
+        for i in range(currentIdx+1, len(list)):
+            if list[smallestIdx] > list[i]:
+                smallestIdx = i
+        list[currentIdx], list[smallestIdx] = list[smallestIdx], list[currentIdx]
+        currentIdx += 1
+    return list
+        
+
+
+
+
+print(selectionSort([1002,-10,-123,0,1,93,21,42]))
