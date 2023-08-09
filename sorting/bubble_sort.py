6610f4d85a630038df273e2ea45ee7c19bc81aa1
Joshua Bizinyika <joshuabiyinzika22@gmail.com>
2023-08-09 10:40:57 +0300

    finished bubble sort

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -1,3 +1,11 @@
 def bubbleSort(list):
-    for idx in range(0, len(list)):
-        pass
+    isSorted = False
+    while not isSorted:
+        isSorted = True
+        for i in range(len(list)-1):
+            if list[i] > list[i+1]:
+                list[i], list[i+1] = list[i+1], list[i]
+                isSorted = False
+    return list
+
+print(bubbleSort([3,1,100,-123,23,17,0,-923]))
