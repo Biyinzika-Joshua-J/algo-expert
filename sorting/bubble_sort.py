fc0289093ab3ca5d0c6ba92a3320cf9116608cb5
Joshua Bizinyika <joshuabiyinzika22@gmail.com>
2023-08-09 11:05:49 +0300

    finished insertion sort

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -1,3 +1,8 @@
+"""
+    time complexity O(n^2)
+    space complexity O(1)
+"""
+
 def bubbleSort(list):
     isSorted = False
     while not isSorted:
@@ -8,4 +13,16 @@ def bubbleSort(list):
                 isSorted = False
     return list
 
-print(bubbleSort([3,1,100,-123,23,17,0,-923]))
+def bubbleSortOptimized(list): # does not affect the time complexity
+    isSorted = False
+    counter = 0
+    while not isSorted:
+        isSorted = True
+        for i in range(len(list)-1-counter):
+            if list[i] > list[i+1]:
+                list[i], list[i+1] = list[i+1], list[i]
+                isSorted = False
+        counter += 1
+    return list
+
+print(bubbleSortOptimized([3,1,100,-123,23,17,0,-923]))
--- sorting/insertion_sort.py
+++ sorting/insertion_sort.py
@@ -1,3 +1,16 @@
+"""
+    not optimal
+    time: O(n^2)
+    space: O(1)
+"""
+
 def insertionSort(list):
-    pass
+    for i in range(1, len(list)):
+        j = i
+        while j > 0 and list[j] < list[j - 1]:
+            list[j], list[j - 1] = list[j - 1], list[j]
+            j -= 1
+    return list
+
+print(insertionSort([848,-19,93,0,-10,1,2]))
 
