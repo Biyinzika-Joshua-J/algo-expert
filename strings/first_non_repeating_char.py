4045f0839b7ff51fcbbcee1d6886edbfa91aba1a
Joshua Bizinyika <joshuabiyinzika22@gmail.com>
2023-08-24 15:09:02 +0300

    small correction

--- strings/first_non_repeating_char.py
+++ strings/first_non_repeating_char.py
@@ -1,4 +1,16 @@
-def first_non_repeating_char():
-    pass
+def first_non_repeating_char(string):
+    chars_hash = {}
+    for letter in string:
+        if letter not in chars_hash:
+            chars_hash[letter] = 0
+        chars_hash[letter] += 1
+    for idx, letter in enumerate(string):
+        if chars_hash[letter] == 1:
+            return idx
+    return -1
 
-print(first_non_repeating_char())
+
+
+string = 'abcdcaf'
+string2='aaabbcc'
+print(first_non_repeating_char(string))
--- strings/run_length_encoding.py
+++ strings/run_length_encoding.py
@@ -4,19 +4,9 @@
 """
 
 def run_length_encoding(string):
-    chars_hash = {}
-    for letter in string:
-        if letter not in chars_hash:
-            chars_hash[letter] = 0
-        chars_hash[letter] += 1
-    for idx, letter in enumerate(string):
-        if chars_hash[letter] == 1:
-            return idx
-    return -1
+    pass
 
 
 
-string = 'abcdcaf'
-string2='aaabbcc'
 
-print(run_length_encoding(string2))
+print(run_length_encoding())
