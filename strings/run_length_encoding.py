3261d3feff73473380c638ae3fcb1c79a3d2c252
Joshua Bizinyika <joshuabiyinzika22@gmail.com>
2023-08-24 15:04:35 +0300

    finished non repeating char

--- strings/run_length_encoding.py
+++ strings/run_length_encoding.py
@@ -1,4 +1,22 @@
-def run_length_encoding():
-    pass
+"""
+    input: string
+    output: return first non repeating character index
+"""
 
-print(run_length_encoding())
+def run_length_encoding(string):
+    chars_hash = {}
+    for letter in string:
+        if letter not in chars_hash:
+            chars_hash[letter] = 0
+        chars_hash[letter] += 1
+    for idx, letter in enumerate(string):
+        if chars_hash[letter] == 1:
+            return idx
+    return -1
+
+
+
+string = 'abcdcaf'
+string2='aaabbcc'
+
+print(run_length_encoding(string2))
