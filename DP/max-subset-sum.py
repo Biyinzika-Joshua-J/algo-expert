bcadaa95dbcc7968ac669dd4306adbc9b1919b8f
Joshua Bizinyika <joshuabiyinzika22@gmail.com>
2023-09-27 11:12:08 +0300

    max sum subset

---
+++ DP/max-subset-sum.py
@@ -0,0 +1,13 @@
+"""
+array of only positive integers
+greatest sum that can be generated without added 2 numbers next to each other in the array
+"""
+
+def max_subset_sum(array):
+    maxSums = [0 for sum in array]
+    maxSums[0] = array[0]
+    maxSums[1] = max(array[0], array[1])
+    for idx in range(2, len(array)):
+        maxSums[idx] = max(maxSums[idx-1], maxSums[idx-2] + array[idx])
+    print(maxSums)
+print(max_subset_sum([7,10,12,7,9,14])) # solution 7 + 12 + 14 = 33
--- DP/min-number-of-coins.py
+++ DP/min-number-of-coins.py
@@ -4,7 +4,7 @@
     find minimum number of coins needed to reach the target amount
     Assume we have an infinite number of coins
 """
-
+# O(ND) time
 def min_num(target, coins):
     numOfCoins = [float('inf') for amount in range(0, target+1)]
     numOfCoins[0] = 0
@@ -12,7 +12,8 @@ def min_num(target, coins):
         for amount, num in enumerate(numOfCoins):
             if coin <= amount: 
                 numOfCoins[amount] = min(numOfCoins[amount], 1+numOfCoins[amount-coin])
-    return numOfCoins[-1]
+    return numOfCoins[-1] if numOfCoins[-1] != float('inf') else -1
 
 
 print(min_num(6, [1,2,4])) # 2 coins 2+4
+
