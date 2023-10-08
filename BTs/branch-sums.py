a18f934be3ae80f2fd65cb18b67de1c6cf66f1dc
Joshua Bizinyika <joshuabiyinzika22@gmail.com>
2023-10-08 09:16:48 +0300

    bts

--- BTs/branch-sums.py
+++ BTs/branch-sums.py
@@ -1,4 +1,9 @@
-def branchSums(root):
-    # pre order, inorder, post order
-    pass
+def branchSums(root, sums=[], runningSum=0):
+    if root is None:
+        return
+    if root.left is None and root.right is None:
+        sums.append(runningSum)
+        return sums
 
+    return [branchSums(root.left, sums, runningSum + root.val), 
+            branchSums(root.right, sums, runningSum + root.val)]
