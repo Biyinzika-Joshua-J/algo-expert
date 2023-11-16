af961f1682ed357563054864de17e3e958ed43a9
Joshua Bizinyika <joshuabiyinzika22@gmail.com>
2023-11-16 11:18:14 +0300

    finished powerset

--- recursion/powerset.py
+++ recursion/powerset.py
@@ -3,5 +3,10 @@ powerset is the set of all subsets of a set
 order of the subsets doesn't matter
 """
 
-def powerset(set):
-    pass
+def powerset(nums):
+    subsets = [[]]
+    for num in nums:
+        for i in range(len(subsets)):
+            currentSubSet = subsets[i]
+            subsets.append(currentSubSet + [num])
+    return subsets
