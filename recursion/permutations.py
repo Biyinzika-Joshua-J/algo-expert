f1b9530344a1f2c369ae96972840ef0a291093aa
Joshua Bizinyika <joshuabiyinzika22@gmail.com>
2023-11-16 10:47:49 +0300

    powerset

---
+++ recursion/permutations.py
@@ -0,0 +1,9 @@
+"""
+    Given an array of unique integers, write a function to return all the permutations 
+    of the integers.
+    
+    definition: A permutation is a unique ordering of the integers
+"""
+
+def permutations(array):
+    pass
---
+++ recursion/powerset.py
@@ -0,0 +1,7 @@
+"""
+powerset is the set of all subsets of a set
+order of the subsets doesn't matter
+"""
+
+def powerset(set):
+    pass
